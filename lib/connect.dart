ed8760376cad452c5abae4d47ab9aed8b8d9a64c
saisrinivaspaila <pailasaisrinivas7668>
2019-12-03 20:38:17 +0530

    Adding Connections Page

--- lib/connect.dart
+++ lib/connect.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 class Connect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -20,7 +20,58 @@ class Connect extends StatelessWidget {
         ),
         body:Container(
           color: Colors.black,
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    "< Contact Details ></>",style: TextStyle(fontSize: 25,color: Colors.white)
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20,top:20),
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Icon(
+                        Icons.mail
+                      ),
+                    ),
+                    FlatButton(onPressed: () => _launchURL('pailasaisrinivas7668@gmail.com','',''),
+                      child: Text('pailasaisrinivas7668@gmail.com', style: TextStyle(fontSize: 20,color: Colors.white)),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20,top:20),
+                child: Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      child: Icon(
+                          Icons.phone
+                      ),
+                    ),
+                    FlatButton(onPressed: () =>{},
+                      child: Text('9121317668', style: TextStyle(fontSize: 20,color: Colors.white)),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         )
     );
+
+  }
+  _launchURL(String toMailId,  String subject, String body) async {
+    var url = 'mailto:$toMailId?subject=$subject&body=$body';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
